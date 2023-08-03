856e206c2bb7f35aa11c85316f723101b93b3083
miguel-barros-clear <miguel.barros.clear@gmail.com>
2023-08-03 07:34:26 -0300

    feat: created mine field component

--- mines/App.tsx
+++ mines/App.tsx
@@ -3,30 +3,41 @@ import React, {Component} from 'react';
 import Field from './src/components/Field';
 import params from './src/params';
 import Flag from './src/components/Flag';
-
+import MineField from './src/components/MineField';
+import {createMineBoard} from './src/functions';
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = this.createState();
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount();
+    const rows = params.getRowsAmount();
+    return Math.ceil(cols * rows * params.difficultLevel);
+  };
+
+  createState = () => {
+    const cols = params.getColumnsAmount();
+    const rows = params.getRowsAmount();
+    return {
+      board: createMineBoard(rows, cols, this.minesAmount()),
+    };
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Iniciando o Mines!!!</Text>
-        <Text>
+        <Text style={styles.welcome}>Iniciando o Mines!!!</Text>
+        <Text style={styles.instructions}>
           Tamanho da grade:
           {params.getRowsAmount()}x{params.getColumnsAmount}
         </Text>
-        <Field />
-        <Field opened />
-        <Field opened nearMines={1} />
-        <Field opened nearMines={2} />
-        <Field opened nearMines={3} />
-        <Field opened nearMines={6} />
-        <Field mined />
-        <Field mined opened />
-        <Field mined opened exploded />
-        <Field flagged opened />
       </View>
     );
   }
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,

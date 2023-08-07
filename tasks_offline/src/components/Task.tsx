f58180d4b3bb47902c72e53d7bfe5d030fde6912
miguel-barros-clear <miguel.barros.clear@gmail.com>
2023-08-07 07:04:21 -0300

    feat: created toggle task function

--- tasks_offline/src/components/Task.tsx
+++ tasks_offline/src/components/Task.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableWithoutFeedback} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import moment from 'moment';
 import 'moment/locale/pt-br';
-
 import commonStyles from '../commonStyles';
-
 export default props => {
   const doneOrNotStyle =
     props.doneAt != null ? {textDecorationLine: 'line-through'} : {};
-
   const date = props.doneAt ? props.doneAt : props.estimateAt;
   const formattedDate = moment(date).locale('pt-br').format('ddd, D [de] MMMM');
 
   return (
     <View style={styles.container}>
-      <View style={styles.checkContainer}>{getCheckView(props.doneAt)}</View>
+      <TouchableWithoutFeedback onPress={() => props.toggleTask(props.id)}>
+        <View style={styles.checkContainer}>{getCheckView(props.doneAt)}</View>
+      </TouchableWithoutFeedback>
       <View>
         <Text style={[styles.desc, doneOrNotStyle]}>{props.desc}</Text>
         <Text style={styles.date}>{formattedDate}</Text>

f86e2a5c9f0bba932aa8718f06f822b8e77c8dff
miguel-barros-clear <miguel.barros.clear@gmail.com>
2023-08-14 07:23:04 -0300

    feat: created login in context

--- lambe/src/data/contexts/UserContext.tsx
+++ lambe/src/data/contexts/UserContext.tsx
@@ -1,21 +1,22 @@
 import React, {createContext, useState} from 'react';
-
 const UserContext = createContext({});
-
 export const UserProvider = ({children}) => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
 
-  const feedInternalContext = {
+  const userInternalContext = {
     name,
     email,
+    login: function (email, password) {
+      setName('Temporario');
+      setEmail(email);
+    },
   };
 
   return (
-    <UserContext.Provider value={feedInternalContext}>
+    <UserContext.Provider value={userInternalContext}>
       {children}
     </UserContext.Provider>
   );
 };
-
 export default UserContext;
